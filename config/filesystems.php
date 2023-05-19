a0325cc9c34a5e78f804e384e3238c026c7fd620
hhannewijk <git-bot@inventivetalent.org>
2023-05-19 19:58:09 +0200

    change filesystem path to public so vue can access it

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path('assets/images'),
+            'url' => env('APP_URL') . 'assets/images',
             'visibility' => 'public',
             'throw' => false,
         ],
